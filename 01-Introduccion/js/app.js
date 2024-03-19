753f23cf25ac206e1e40cd9cb790946d077b7956
Carlos Ramirez <karlossander92@gmail.com>
2024-03-18 20:29:23 -0500

    Introduccion

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,15 @@
+// Pedimos el nombre por el promp de JS
+const nombre = prompt('Cual es tu nombre ?')
+// Remplazamos el texto en una etiqueta de HTML con JavaScript
+document.querySelector('.contenido  ').innerHTML = `${nombre} está aprendiendo JS moderno`
+
+/* Comanetario
+de
+varias 
+lineas
+en 
+JS
+ */
+
+
+console.warn('Eso no esta permitido')
