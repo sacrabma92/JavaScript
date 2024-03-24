c7ef0e7ad9b1ba256bdebeb333fa25028795fca2
Carlos Ramirez <karlossander92@gmail.com>
2024-03-23 22:25:49 -0500

    Iteradores

--- 11-Iteradores/js/03-app.js
+++ 11-Iteradores/js/03-app.js
@@ -1,16 +1,15 @@
-//Fizz buzz 100 NUMEROS
+// Fizz Buzz...
 
-//3 6 9 12... fizz
-//5 10 15 20 .... buzz
-//15 30 45 ...FIZBUZZ
+// 3 6 9 12 15 ...  FIZZ
+// 5 10 15 20 25 ... BUZZ
+// 15 30 45 ... Fizz Buzz
 
-for(let i = 0;i < 100; i++){
-    if(i%3 === 0 && i%5 ===0) {
-        console.log(`${i} FizzBuzz`)
-    }
-    else if( i%3 === 0){
-        console.log(`${i} Fizz`)
-    }else if(i%5 === 0){
-        console.log(`${i} Buzz`)
-    }
+for(let i = 1; i < 100; i++  ) {
+    if(i % 15 === 0) {
+        console.log(`${i} FIZZ BUZZ`)
+    } else if(i % 3 === 0) {
+        console.log(`${i} fizz`);
+    } else if ( i % 5 === 0 ) {
+        console.log(`${i} buzz`)
+    } 
 }
