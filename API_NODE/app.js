93182ae42840f08754f49a906e7b92a8f0f0a760
Carlos Andres Ramirez <91298237+sacrabma92@users.noreply.github.com>
2024-05-24 21:42:17 -0500

    API REST Node

---
+++ API_NODE/app.js
@@ -0,0 +1,19 @@
+// Importamos la libreria dotenv para poder usar las variables de entorno
+require('dotenv').config();
+// Importamos la libreria express
+const express = require('express');
+// Importamos la libreria cors
+const cors = require('cors');
+// Creamos la variable app que contendra la aplicacion
+const app = express();
+
+// Le asigano a la variable app el uso de la libreria cors
+app.use(cors());
+
+// Definimos el puerto en el que se ejecutara la aplicacion
+const port = process.env.PORT || 3000;
+
+// Definimos app.listen para que la aplicacion se ejecute en el puerto definido
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
